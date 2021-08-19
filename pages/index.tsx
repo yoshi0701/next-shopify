5047d731be3d71330f612f85945204b1ce2a5713
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-08-20 00:11:56 +0900

    init project

---
+++ pages/index.tsx
@@ -0,0 +1,24 @@
+
+import type { InferGetStaticPropsType} from "next"
+
+export async function getStaticProps() {
+  const products = [1, 2, 3]
+
+  return {
+    props: {
+      products
+    },
+    revalidate: 4 * 60 * 60
+  }
+}
+
+export default function Home({
+  products
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+
+  return (
+    <div>
+      {products}
+    </div>
+  )
+}
