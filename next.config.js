02e5fde5ad903dcb22743d59630a38423b09587c
tomoaki yoshioka <tomoaki.yoshiki@gmail.com>
2021-08-28 19:52:53 +0900

    feat: adding module specific config for switch content

--- next.config.js
+++ next.config.js
@@ -1,3 +1,10 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { withFrameworkConfig } = require("./framework/common/config");
+
+module.exports = withFrameworkConfig({
+  i18n: {
+    locales: ["en-US", "es"],
+    defaultLocale: "en-US",
+  },
+});
+
+console.log("next.config.js", JSON.stringify(module.exports, null, 2));
